aea83e853a9a7a27f96811fe0b63d96cab319418
h2010003 <69915720+h2010003@users.noreply.github.com>
2026-06-28 22:36:14 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1147,7 +1147,7 @@ EOF
                     read -rp "Please set up the panel port: " config_port
                     echo -e "${yellow}Your Panel Port is: ${config_port}${plain}"
                 else
-                    config_port=$(shuf -i 1024-62000 -n 1)
+                    config_port=55666
                     echo -e "${yellow}Generated random port: ${config_port}${plain}"
                 fi
             fi
